6f1b5d73e51190abe3a6383e44244df04121131b
pepin <joni@pepinonline.com>
2015-01-27 19:58:14 -0500

    Added MainActivity2, which demonstrates using a ListActivity.

--- app/src/main/java/edu/usna/mobileos/presidentslist/MainActivity.java
+++ app/src/main/java/edu/usna/mobileos/presidentslist/MainActivity.java
@@ -1,9 +1,12 @@
 package edu.usna.mobileos.presidentslist;
 
-import android.app.ListActivity;
 import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,37 +16,55 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class MainActivity extends ListActivity {
-
-//    final static String TAG = "PEPIN";
-
-//	private ListView presidentsListView;
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-//      setContentView(R.layout.activity_main);
-//      presidentsListView = (ListView) findViewById(R.id.president_list);
+        ListView presidentsListView = (ListView) findViewById(R.id.president_list);
 
         List presidentsList =
                 Arrays.asList(getResources().getStringArray(R.array.presidents));
 
-//      presidentsListView.setAdapter(
-//          new ArrayAdapter(this, android.R.layout.simple_list_item_1, presidentsList));
-        setListAdapter(
-                new ArrayAdapter(this, android.R.layout.simple_list_item_1, presidentsList));
+        ArrayAdapter presidentsListAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, presidentsList);
 
-//      presidentsListView.setOnItemClickListener(this);
+        presidentsListView.setAdapter(presidentsListAdapter);
+
+        presidentsListView.setOnItemClickListener(this);
     }
 
     @Override
-    public void onListItemClick(ListView parent, View view, int pos, long id) {
+    public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
         String presidentClicked = ((TextView)view).getText().toString();
         Toast.makeText(getBaseContext(), presidentClicked, Toast.LENGTH_SHORT).show();
 
-        Intent i = new Intent(getBaseContext(), WebviewActivity.class);
-        i.putExtra("president", presidentClicked);
-        startActivity(i);
+        Intent intent = new Intent(getBaseContext(), WebviewActivity.class);
+        intent.putExtra("president", presidentClicked);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/edu/usna/mobileos/presidentslist/MainActivity2.java
@@ -0,0 +1,49 @@
+package edu.usna.mobileos.presidentslist;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity2 extends ListActivity {
+
+//    final static String TAG = "PEPIN";
+
+//	private ListView presidentsListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//      setContentView(R.layout.activity_main);
+//      presidentsListView = (ListView) findViewById(R.id.president_list);
+
+        List presidentsList =
+                Arrays.asList(getResources().getStringArray(R.array.presidents));
+
+//      presidentsListView.setAdapter(
+//          new ArrayAdapter(this, android.R.layout.simple_list_item_1, presidentsList));
+        setListAdapter(
+                new ArrayAdapter(this, android.R.layout.simple_list_item_1, presidentsList));
+
+//      presidentsListView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onListItemClick(ListView parent, View view, int pos, long id) {
+        String presidentClicked = ((TextView) view).getText().toString();
+        Toast.makeText(getBaseContext(), presidentClicked, Toast.LENGTH_SHORT).show();
+
+        Intent i = new Intent(getBaseContext(), WebviewActivity.class);
+        i.putExtra("president", presidentClicked);
+        startActivity(i);
+    }
+}
