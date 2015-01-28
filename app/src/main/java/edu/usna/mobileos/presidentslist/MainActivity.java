4571463606331239277c775326ec282b9e430711
pepin <joni@pepinonline.com>
2015-01-27 19:43:59 -0500

    Updated to use ListActivity.  (Notice that it is now missing the ActionBar!)

--- app/src/main/java/edu/usna/mobileos/presidentslist/MainActivity.java
+++ app/src/main/java/edu/usna/mobileos/presidentslist/MainActivity.java
@@ -1,12 +1,9 @@
 package edu.usna.mobileos.presidentslist;
 
+import android.app.ListActivity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,55 +13,37 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends ListActivity {
+
+//    final static String TAG = "PEPIN";
+
+//	private ListView presidentsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        ListView presidentsListView = (ListView) findViewById(R.id.president_list);
+//      setContentView(R.layout.activity_main);
+//      presidentsListView = (ListView) findViewById(R.id.president_list);
 
         List presidentsList =
                 Arrays.asList(getResources().getStringArray(R.array.presidents));
 
-        ArrayAdapter presidentsListAdapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, presidentsList);
+//      presidentsListView.setAdapter(
+//          new ArrayAdapter(this, android.R.layout.simple_list_item_1, presidentsList));
+        setListAdapter(
+                new ArrayAdapter(this, android.R.layout.simple_list_item_1, presidentsList));
 
-        presidentsListView.setAdapter(presidentsListAdapter);
-
-        presidentsListView.setOnItemClickListener(this);
+//      presidentsListView.setOnItemClickListener(this);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+    public void onListItemClick(ListView parent, View view, int pos, long id) {
         String presidentClicked = ((TextView)view).getText().toString();
         Toast.makeText(getBaseContext(), presidentClicked, Toast.LENGTH_SHORT).show();
 
-        Intent intent = new Intent(getBaseContext(), WebviewActivity.class);
-        intent.putExtra("president", presidentClicked);
-        startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Intent i = new Intent(getBaseContext(), WebviewActivity.class);
+        i.putExtra("president", presidentClicked);
+        startActivity(i);
     }
 }
